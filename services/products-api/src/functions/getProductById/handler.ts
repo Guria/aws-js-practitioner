5902b4d1bae05e38abd2624ff02ecf00dcd4c5a9
Aleksei Gurianov <Aleksei_Gurianov@epam.com>
2022-10-17 22:11:39 +0300

    task3.2: create lambda function for product item
    :heavy_check_mark: Create a lambda function called `getProductsById` in Product Service which will be triggered by the HTTP GET method.
    :heavy_check_mark: The requested URL should be `/products/{productId}`
    :heavy_check_mark: The response from the lambda should be 1 searched product from an array of products (mock data should be used).
    
    Extra effort:
    * This endpoint is integrated with Frontend app. New route and components set is created
    * Business logic moved from handler code to a service class

--- services/products-api/src/functions/getProductById/handler.ts
+++ services/products-api/src/functions/getProductById/handler.ts
@@ -2,8 +2,9 @@ import type { APIGatewayProxyHandler } from "aws-lambda";
 import { formatJSONResponse, middyfy } from "@libs/api-gateway";
 import productsService from "@services/products";
 
-const handler: APIGatewayProxyHandler = async () => {
-  return formatJSONResponse(await productsService.getProducts());
+const handler: APIGatewayProxyHandler = async (event) => {
+  const { productId } = event.pathParameters;
+  return formatJSONResponse(await productsService.getProductById(productId));
 };
 
 export const main = middyfy(handler);
--- services/products-api/src/services/products.ts
+++ services/products-api/src/services/products.ts
@@ -4,6 +4,10 @@ import { products } from "@guria.dev/aws-js-practitioner-commons/mocks";
 class ProductsService {
   constructor(private products: Product[]) {}
 
+  public getProductById(id: string | number): Product | undefined {
+    return this.products.find((item) => item.id === id);
+  }
+
   public getProducts(): Product[] {
     return this.products;
   }
